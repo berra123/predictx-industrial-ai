b5ab118cc1045044c9cfea34e08b77a74a813d6b
Berra123 <berra.gul1@icloud.com>
2026-07-08 16:34:03 +0300

    Remove streamlit autorefresh for cloud deployment

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_autorefresh import st_autorefresh
 
 # Tema
 from utils.theme import apply_theme
@@ -16,55 +15,27 @@ from app_pages.reports import show_reports
 from app_pages.settings import show_settings
 
 
-# ==========================
-# Theme
-# ==========================
-
 apply_theme()
-
-# ==========================
-# Auto Refresh
-# ==========================
-
-st_autorefresh(
-    interval=2000,
-    key="predictx_refresh"
-)
-
-# ==========================
-# Sidebar
-# ==========================
 
 selected_page = render_sidebar()
 
-# ==========================
-# Navigation
-# ==========================
-
 if selected_page == "📈 Executive Dashboard":
-
     show_executive()
 
 elif selected_page == "🏠 Dashboard":
-
     show_dashboard()
 
 elif selected_page == "🏭 Pulper":
-
     show_pulper()
 
 elif selected_page == "🚨 Alarm Center":
-
     show_alarms()
 
 elif selected_page == "📊 Reports":
-
     show_reports()
 
 elif selected_page == "⚙️ Settings":
-
     show_settings()
 
 else:
-
     st.error("Page not found.")
